4e492f5ef3016f354af9b4a9cffc8582b86f66a2
Sajan4072 <rai4072@gmail.com>
2024-11-01 16:09:59 -0400

    feat:create inventory model

--- Backend/book_inventory/apps/inventory_api/models.py
+++ Backend/book_inventory/apps/inventory_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
 
 # Create your models here.
+class Inventory(models.Model):
+    entry_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=1000)
+    author = models.CharField(max_length=1000)
+    genre = models.CharField(max_length=1000)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+ 
+#string representation of the model
+    def __str__(self):
+        return f"{self.title} by {self.author} isbn {self.isbn} genre {self.genre} published on {self.publication_date}"
+        
+
+ #check if the isbn is valid
+    def clean(self):
+        # ISBN validation to ensure it follows standard format
+        if not re.match(r'^\d{13}$', self.isbn):
+            raise ValidationError('ISBN must be 13 digits.')
